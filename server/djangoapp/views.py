a6d4c1a5b238a01eb89eff579ca90a2455b96824
sandrawar <sandra.a3@o2.pl>
2025-04-29 11:31:59 -0400

    cleanup

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -122,7 +122,8 @@ def add_review(request):
     if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            # response = 
+            post_review(data)
             return JsonResponse({"status": 200})
         except Exception as e:
             return JsonResponse(
